f9664f798e0d756a2d8970fd11c1cbaa38f3c1b1
Manish Mani Dinkar <moneyyiiss@gmail.com>
2022-04-17 16:10:49 +0530

    file_system_organizer
    This is new updated files

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,15 @@
 function helpFn(dirPath) {
     //jish order me likhunga usi order me likhayega back tick se
     console.log(`
-List of All the commands:
-        node main.js tree "directoryPath"
-        node main.js organize "directoryPath"
-        node main.js help "directoryPath"
+    List of All the commands:
+            node main.js tree "directoryPath"
+            node main.js organize "directoryPath"
+            node main.js help "directoryPath"
+    
+    List of command when you use this through command prompt:
+            fis help
+            fis organize "directoryPath"
+            fis tree "directoryPath"
         `)
 }
 
